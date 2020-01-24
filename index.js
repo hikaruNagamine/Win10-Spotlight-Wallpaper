55eb96e3cc7bf44ef5c2b9764ba7b7fa9b900fac
hikaru nagamine <hikaru.nagamine@gmail.com>
2020-01-24 10:14:58 +0900

    copy image

--- index.js
+++ index.js
@@ -4,45 +4,56 @@ const sizeOf = require('image-size')
 const fs = require('fs')
 const path = require('path')
 
-const USER_LOCAL_PATH = process.env.LOCALAPPDATA
 // copy元のフォルダー：windows10 spotlightの保存先パス
 const SPOTLIGHT_IMG_PATH = path.join(
-    USER_LOCAL_PATH,
+    process.env.LOCALAPPDATA,
     'Packages',
     'Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy',
     'LocalState',
     'Assets')
 // copy先のフォルダ
-const TO_FOLDER = path.join(USER_LOCAL_PATH,'Pictures')
+const TO_FOLDER = path.join(process.env.HOMEPATH,'WallpaperPictures')
 
-try {
-    // fileS list
-    fs.readdir(SPOTLIGHT_IMG_PATH, (err, files) => {
-        if(err) throw err
-        let count = 0
-        files.forEach((file)=>{
-            let fromFile = path.join(SPOTLIGHT_IMG_PATH, file)
-            let toFile = path.join(TO_FOLDER, file)
-            count++
-            console.log(count + " : " + file)
-            // read image file
-            let dimensions = sizeOf(fromFile);
-            console.log(dimensions.width, dimensions.height);
+let main = async () => {
+    try {
+        console.log("****** windows10 spotlight image to wallpaper ******")
+        // copy先フォルダを作成する
+        await fs.mkdir(TO_FOLDER, { recursive: true }, (err) => {
+            if (err) throw err;
+        });
+        // file list
+        fs.readdir(SPOTLIGHT_IMG_PATH, (err, files) => {
+            if(err) throw err
+            let count = 0
+            files.forEach((file)=>{
+                let fromFile = path.join(SPOTLIGHT_IMG_PATH, file)
+                let toFile = path.join(TO_FOLDER, file)
+                count++
+                // console.log(count + " : " + file)
+                // read image file
+                let dimensions = sizeOf(fromFile);
+                // console.log(dimensions.width, dimensions.height)
+    
+                // file check : 1980 * 1080 size
+                if(dimensions.width != 1920) {
+                    return
+                }
+    
+                // file check : check existence
+                // TODO
 
-            // file check : 1980 * 1080 size
-            if(dimensions.width != 1920) {
-                return
-            }
-
-            // file check : check existence
-            
-            // file copy
-            fs.copyFile(fromFile, toFile + '.' + dimensions.type,(err) => {
-                if (err) throw err;
-            });
-              
+                // file copy
+                fs.copyFile(fromFile, toFile + '.' + dimensions.type,(err) => {
+                    if (err) throw err;
+                    console.log(count + " : " + file)
+                    console.log(dimensions.width, dimensions.height)
+                });
+                  
+            })
         })
-    })
-} catch (e) {
-    console.error(e)
+    } catch (e) {
+        console.error(e)
+    }
 }
+
+main();
